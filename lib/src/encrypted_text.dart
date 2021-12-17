920323f216af030f3836b1e9cc3a9166f607603f
Ahmet Safa Bülbül <ahmetsafabulbul@gmail.com>
2021-12-17 19:43:37 +0300

    unicode support is removed

--- lib/src/encrypted_text.dart
+++ lib/src/encrypted_text.dart
@@ -4,11 +4,7 @@ import 'package:spn_16_encryption/src/message.dart';
 
 class EncryptedText extends Message {
   EncryptedText(String text) : super(text);
-
-  @override
-  EncryptedText.fromBytes(List<int> bytes) : super.fromBytes(bytes) {
-    super.text = utf8.decode(bytes);
-  }
+  EncryptedText.fromBytes(List<int> bytes) : super.fromBytes(bytes);
 
   List<int> get asBytes => super.bytes;
   String get asText => super.text;
--- test/spn_16_encryption_test.dart
+++ test/spn_16_encryption_test.dart
@@ -19,11 +19,4 @@ void main() {
     final spn = SPN16();
     expect(spn.encrypt(plainText: "Selam", keyword: "parola"), "Failed");
   });
-
-  /*test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });*/
 }
